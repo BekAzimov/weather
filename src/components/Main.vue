d402030441899148b18aea870427828c204f1b58
Bek <3209094@gmail.com>
2023-09-05 20:15:11 +0500

    finished Weather

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <Current/>
+        <Week/>
     </div>
 </template>
 
 <script>
 import Current from './Current/Current.vue'
+import Week from './Week/Week.vue'
+
     export default {
         components: {
-            Current
+            Current,
+            Week
         }
     }
 </script>
